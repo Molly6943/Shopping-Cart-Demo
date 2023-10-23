31e47d8d69ace677b7fa8018dfaed33baa4bc0ef
mengmengliu <mengmengliu@aliyun.com>
2023-10-23 23:13:47 +0800

    add CRUD

---
+++ scripts/data.js
@@ -0,0 +1,83 @@
+const MENU_JSON_BIN_ID = '653631d754105e766fc5dfd2'
+const BASE_URL = 'https://api.jsonbin.io/v3'
+const MASTER_KEY = '$2a$10$elUWikH0smucd2oF9jhSWOT4ZDF0eFmGRefbU9kUX1fgIyHoX3IBC'
+const SHOPPING_CART_BIN_ID = '65366c1054105e766fc5f4c4'
+//example url: https://api.jsonbin.io/v3/b/653631d754105e766fc5dfd2
+
+async function getMenu () {
+  try{
+    const response = await axios.get(`${BASE_URL}/b/${MENU_JSON_BIN_ID}/latest`, {headers: {
+      "X-Master-Key": MASTER_KEY
+    }})
+    return response.data.record
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+async function getShoppingCartList() {
+  try{
+    const response = await axios.get(`${BASE_URL}/b/${SHOPPING_CART_BIN_ID}/latest`, {headers: {
+      "X-Master-Key": MASTER_KEY
+    }})
+    if (response.data && response.data.record) {
+      return response.data.record
+    } else {
+      return []
+    }
+    
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+async function createShoppingCart (data) {
+  try{
+    let cartList = await getShoppingCartList()
+    if(cartList.length > 0){
+      cartList.push(data)
+      const params = cartList
+
+      await axios.put(`${BASE_URL}/b/${SHOPPING_CART_BIN_ID}`, params, {headers: {
+        "X-Master-Key": MASTER_KEY,
+        "Content-Type": "application/json"
+      }})
+    }
+    
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+async function updateShoppingCart (cartList, data) {
+  try{
+    const params = cartList.map((x) => {
+      if (x.id === data.id) {
+        x = data
+      }
+      return x
+    })
+    await axios.put(`${BASE_URL}/b/${SHOPPING_CART_BIN_ID}`, params, {headers: {
+      "X-Master-Key": MASTER_KEY,
+      "Content-Type": "application/json"
+    }})
+  } catch(error) {
+    throw new Error(error)
+  }
+}
+
+
+async function deleteShoppingCart(cartList, id) {
+  console.log('cartList, id', cartList, id)
+  try{
+    if (cartList.length > 0) {
+      const params = cartList.filter((x) => x.id !== id)
+      await axios.put(`${BASE_URL}/b/${SHOPPING_CART_BIN_ID}`, params, {headers: {
+        "X-Master-Key": MASTER_KEY,
+        "Content-Type": "application/json"
+      }})
+    }
+  }catch(error) {
+    throw new Error(error)
+  }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,90 @@
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+async function renderMenu() {
+    const menuListElement = document.querySelector('.menu-list')
+    const menuList = await getMenu()
+    if (menuList.length > 0) {
+        menuList.forEach((item) => {
+          const listItem = document.createElement('div')
+          listItem.classList = "card mb-3 me-3"
+          listItem.innerHTML = 
+            `<div class="row g-0 d-flex align-items-center">
+                  <div class="col-md-8">
+                      <div class="card-body">
+                          <h6 class="card-title">${item.name}</h6>
+                          <p class="card-text">
+                              <small class="text-muted">${item.desc}</small>
+                          </p>
+                          <p class="card-text">
+                              S$ ${ (item.priceCents / 100).toFixed(2)}
+                          </p>
+                      </div>
+                  </div>
+                  <div class="col-md-4 pe-2">
+                      <img
+                          src="${item.image}"
+                          class="img-fluid rounded"
+                          alt="...">
+                  </div>
+              </div>
+              <div class="add-to-card">
+                  <i class="fa-solid fa-plus"></i>
+              </div>`
+
+            listItem.querySelector('.add-to-card').addEventListener('click', async () => {
+              const params = Object.assign(item, {quantity: 1})
+              await createShoppingCart(params)
+              renderShoppingCart()
+            })
+            menuListElement.appendChild(listItem);
+      });
+    }
+}
+
+async function renderShoppingCart () {
+  const cartListElement = document.querySelector('.cart-list')
+  cartListElement.innerHTML = ""
+  const cartList = await getShoppingCartList()
+  if (cartList.length > 0) {
+    cartList.forEach((item) => {
+      const listItem = document.createElement('div')
+      listItem.classList = 'cart-item'
+      listItem.innerHTML = `
+        <div class="cart-item-img">
+            <img
+                src="${item.image}"
+                class="img-fluid rounded"
+                alt="...">
+        </div>
+        <h6 class="red cart-title">${item.name}</h6>
+        <div>
+            <div>
+                <p>
+                    <small class="text-muted">S$ ${ (item.priceCents / 100).toFixed(2)}</small>
+                </p>
+                <div class="quantity">
+                    <span id="delete-cart-item">
+                        <i class="fa-regular fa-trash-can red"></i>
+                    </span>
+                    <input disabled="disabled" value="${item.quantity}"/>
+                    <span id="add-cart-item">
+                        <i class="fa-solid fa-plus red"></i>
+                    </span>
+                </div>
+            </div>
+        </div>`
+      listItem.querySelector('#delete-cart-item').addEventListener('click', async () => {
+        await deleteShoppingCart(cartList, item.id)
+        renderShoppingCart()
+      })
+      listItem.querySelector('#add-cart-item').addEventListener('click', async () => {
+        const params = Object.assign(item, {quantity: item.quantity+1})
+        await updateShoppingCart(cartList, params)
+        renderShoppingCart()
+      })
+      cartListElement.appendChild(listItem);
+    })
+  }
+}
+
+renderMenu()
+
+renderShoppingCart()
